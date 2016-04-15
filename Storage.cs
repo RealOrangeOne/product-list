f32cf745b6f775aee406d5757ed7928f00262a01
Jake Howard <git@theorangeone.net>
2016-04-15 11:58:13 +0100

    Add storage class to store list in memory

---
+++ Storage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace productlist
+{
+	public static class Storage
+	{
+		private static List<Product> productStorageList = new List<Product>();
+
+		public static void Add(Product p) {
+			productStorageList.Add (p);
+		}
+
+		private static void listProducts() {
+			if (productStorageList.Count < 1) {
+				Console.WriteLine ("No Products found!");
+				return;
+			}
+			foreach (Product p in productStorageList) {
+				p.display ();
+			}
+		}
+
+		public static void List() {
+			listProducts ();
+		}
+	}
+}
+
